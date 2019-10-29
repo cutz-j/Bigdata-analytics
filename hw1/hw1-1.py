850cb4f843d2e28341f490bcf844952b957e0569
cutz-j <cutz-j@naver.com>
2019-10-29 18:08:46 +0900

    Update

--- hw1/hw1-1.py
+++ hw1/hw1-1.py
@@ -7,50 +7,54 @@ from sklearn.metrics import f1_score, confusion_matrix
 import warnings
 warnings.filterwarnings("ignore")
 
-# Seed 0
-np.random.seed(0)
-
-########## Data Load ###########
+######### Preprocess ##########
+# Data Load
 data = np.loadtxt('./data/SvmData.txt')
 data.shape
 
-# Permutation
+# 2) Permutation
+# 2) Seed 0
+np.random.seed(0)
 n_sample = len(data)
 order = np.random.permutation(n_sample)
 data = data[order]
 
-# Column split
+# 1) Column split
 x = data[:,:7]
 y = data[:, [-1]]
 
-
+# 3) Data split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, stratify=y, random_state = 42)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.25, stratify=y_train, random_state = 42)
 
-
+# 4) Scaling
 ss = StandardScaler()
 X_train = ss.fit_transform(x_train)
 X_val = ss.transform(x_val)
 X_test = ss.transform(x_test)
 
-
+########## Validation ##########
+# 1) Kernel parameters
 param_list = np.linspace(start=0.1, stop=1, num=10)
 kernel_list = ['linear', 'rbf', 'poly', 'sigmoid']
 
 for kernel in kernel_list: # kernel function
     f1_list = []
     for p in param_list: # parameter choice
+        # 2) C & Gamma parameter validation
         svc = SVC(kernel=kernel, C=p if kernel=='linear' else 1.0, gamma='auto_deprecated' if kernel=='linear' else p)
         svc.fit(X_train, y_train)
         y_hat = svc.predict(X_val)
         f1_list.append(f1_score(y_val, y_hat, average='micro'))
     
+########## Test ###########
+    # 1) SVM fit
     best_f1 = param_list[np.argmax(f1_list)]
     svc_model = SVC(kernel=kernel, C=best_f1 if kernel=='linear' else 1.0, gamma='auto_deprecated' if kernel=='linear' else best_f1)
     svc_model.fit(X_train, y_train)
     y_hat = svc_model.predict(X_test)
     
-    # result
+    # 2) Print
     print("%s kernel SVM Best parameter C: %f" %(kernel, best_f1))
     print("%s kernel SVM F1 score : %f" %(kernel, f1_score(y_test, y_hat, average='micro')))
     print("%s kernel Confusion Matrix : \n" %(kernel), confusion_matrix(y_test, y_hat))
--- hw1/hw1-2.py
+++ hw1/hw1-2.py
@@ -5,26 +5,35 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
-np.random.seed(0)
+############# Preprocess #############
+# Data load
 data = np.loadtxt('data/RegressionData.txt', delimiter=',')
+# 2) Permutation & Seed 0
+np.random.seed(0)
 n_sample = len(data)
 order = np.random.permutation(n_sample)
 data = data[order]
 
+# 1) Column split
 x_data = data[:,:-2]
 y_data = data[:,-2:]
 
+# 3) Data split
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.2, random_state = 42)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.25, random_state = 42)
 
+# 4) Scaling
 mms = MinMaxScaler()
 X_train = mms.fit_transform(x_train)
 X_val = mms.transform(x_val)
 X_test = mms.transform(x_test)
 
+################ Validation #################
+# 2) alpha list
 alpha_list = [0.0001, 0.001, 0.01, 0.1, 1]
 label = ['latitute', 'longitude']
 
+# 1) Except for LS, validation
 for i in range(2): # 0: latitute, 1: longitude
     for model in ['LeastSquare', 'Ridge', 'Lasso']:
         mse_list = []
@@ -39,6 +48,7 @@ for i in range(2): # 0: latitute, 1: longitude
             y_hat = rm.predict(X_val)
             mse_list.append(mean_squared_error(y_val[:,i], y_hat))
         
+################## Test ######################
         if model == 'LeastSquare':
             rm = linear_model.LinearRegression()
         elif model == 'Ridge':
@@ -51,6 +61,7 @@ for i in range(2): # 0: latitute, 1: longitude
         rmse = np.sqrt(mean_squared_error(y_test[:,i], y_hat))
         var = r2_score(y_test[:,i], y_hat)
         
+        # 2) Print
         if model == 'Ridge' or model == 'Lasso':
             print("%s best alpha of %s: %f" %(model, label[i], alpha_list[np.argmin(mse_list)]))
         print("%s RMSE of %s: %f" %(model, label[i], rmse))
